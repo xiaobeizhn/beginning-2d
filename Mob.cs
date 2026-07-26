4d183c97f079506696f31a36fc5a0db1e3e990f8
曾浩楠 <2395812418@qq.com>
2026-07-26 23:50:54 +0800

    init

---
+++ Mob.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Mob : RigidBody2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
+		animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
+	}
+
+	// We also specified this function name in PascalCase in the editor's connection window.
+	private void OnVisibleOnScreenNotifier2DScreenExited()
+	{
+		QueueFree();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
